36896403b0b58ed8741f09f843f671c0a1f7cf8e
andre-rodrigues-correia <andrecorreiati@gmail.com>
2022-09-09 11:15:40 -0300

    config knex and mingranions in env test

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+    test: {
+        client: 'pg',
+        version: '14.5',
+        connection: {
+            host: 'localhost',
+            user: 'postgres',
+            password: '1234',
+            database: 'api-database',
+        },
+        // migrations: {
+        //     directory: 'src/migrations',
+        // },
+    },
+};
