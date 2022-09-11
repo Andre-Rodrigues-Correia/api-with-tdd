ea765714ed858f241b23d881527afb9ca98a3bf6
andre-rodrigues-correia <andrecorreiati@gmail.com>
2022-09-10 21:37:19 -0300

    create route accounts

---
+++ migrations/20220910225551_create_table_accounts.js
@@ -0,0 +1,14 @@
+exports.up = (knex) => {
+    return knex.schema.createTable('accounts', (t) => {
+        t.increments('id').primary();
+        t.string('name').notNullable();
+        t.integer('user_id')
+            .references('id')
+            .inTable('users')
+            .notNullable();
+    });
+};
+
+exports.down = (knex) => {
+    return knex.schema.dropTable('accounts');
+};
